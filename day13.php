13bb0e8f76d31cf4a64854ab248c49c23a674da0
Alen Oblak <alen.oblak@gmail.com>
2023-12-13 18:43:39 +0100

    Optimized day 13 solution

--- day13.php
+++ day13.php
@@ -17,49 +17,13 @@ $patterns[] = $pattern;
 $sum1 = 0;
 $sum2 = 0;
 foreach ($patterns as $pattern) {
-    $sum1 += calc($pattern);
-    $sum2 += calc2($pattern);
+    $sum1 += calc($pattern, 0);
+    $sum2 += calc($pattern, 1);
 }
 echo $sum1."\n";
 echo $sum2."\n";
 
-function calc($pattern)
-{
-    $x = count($pattern[0]);
-    $y = count($pattern);
-
-    // try x
-    for($xx = 1; $xx < $x; $xx++) {
-        $is_mirror = true;
-        $length = min($xx, $x - $xx);
-        for($i = 1; $i <= $length; $i++)
-            for($j = 0; $j < $y; $j++) {
-                if($pattern[$j][$xx-$i] != $pattern[$j][$xx+$i-1] ) {
-                    $is_mirror = false;
-                    break 2;
-                }
-            }
-        if($is_mirror)
-            return $xx;
-    }
-
-    // try y
-    for($yy = 1; $yy < $y; $yy++) {
-        $is_mirror = true;
-        $length = min($yy, $y - $yy);
-        for($i = 0; $i < $x; $i++)
-            for($j = 1; $j <= $length; $j++) {
-                if($pattern[$yy-$j][$i] != $pattern[$yy+$j-1][$i] ) {
-                    $is_mirror = false;
-                    break 2;
-                }
-            }
-        if($is_mirror)
-            return $yy * 100;
-    }
-}
-
-function calc2($pattern)
+function calc($pattern, $max_diff)
 {
     $x = count($pattern[0]);
     $y = count($pattern);
@@ -72,11 +36,11 @@ function calc2($pattern)
             for($j = 0; $j < $y; $j++) {
                 if($pattern[$j][$xx-$i] != $pattern[$j][$xx+$i-1] ) {
                     $diff++;
-                    if($diff > 1)
+                    if($diff > $max_diff)
                         break 2;
                 }
             }
-        if($diff == 1)
+        if($diff == $max_diff)
             return $xx;
     }
 
@@ -88,11 +52,11 @@ function calc2($pattern)
             for($j = 1; $j <= $length; $j++) {
                 if($pattern[$yy-$j][$i] != $pattern[$yy+$j-1][$i] ) {
                     $diff++;
-                    if($diff > 1)
+                    if($diff > $max_diff)
                         break 2;
                 }
             }
-        if($diff == 1)
+        if($diff == $max_diff)
             return $yy * 100;
     }
 }
